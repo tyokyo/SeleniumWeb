64b24e898fa7c93f3b6f8b19848e370418ac1223
qiang.zhang <qiang.zhang@ck-telecom.com>
2017-08-18 14:44:08 +0800

    test

---
+++ src/run/XmlRunner.java
@@ -0,0 +1,36 @@
+package run;
+import java.util.ArrayList;
+import java.util.List;
+import org.testng.TestNG;
+import org.uncommons.reportng.HTMLReporter;
+import org.uncommons.reportng.JUnitXMLReporter;
+import listener.TestngListener;
+
+public class XmlRunner {
+	private static final String ESCAPE_PROPERTY = "org.uncommons.reportng.escape-output";
+	public static void main(String[] args) throws Exception {
+		System.setProperty(ESCAPE_PROPERTY, "false"); 
+		// TODO Auto-generated method stub
+		TestNgXml.startMakeReport();
+		
+		TestNG testNG = new TestNG();
+		testNG.setUseDefaultListeners(false);
+		testNG.addListener(new HTMLReporter());
+		testNG.addListener(new JUnitXMLReporter());
+		testNG.addListener(new TestngListener());
+		//testNG.addListener(new OSFilter());
+		//testNG.addListener(new IProgressTracker());
+		//testNG.setThreadCount(2);
+		
+		//testNG.setSuiteThreadPoolSize(2);
+		testNG.setOutputDirectory(TestNgXml.reportFolder);
+		List<String> suits = new ArrayList<String>();
+		suits.add("xml/HomePageCase.xml");
+		testNG.setTestSuites(suits);
+		testNG.run();
+		System.out.println(testNG.getOutputDirectory());
+		//copy report
+		TestNgXml.endMakeReport();
+	}
+}
+
