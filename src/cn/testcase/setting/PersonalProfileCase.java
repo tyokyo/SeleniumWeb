4baa6117cd63df1e93f79b1d0a9170471f2419c6
qiang.zhang <qiang.zhang@ck-telecom.com>
2017-09-19 11:14:32 +0800

    Owner-commite-04

--- src/cn/testcase/setting/PersonalProfileCase.java
+++ src/cn/testcase/setting/PersonalProfileCase.java
@@ -14,6 +14,7 @@ import model.VP;
 import model.WaitCondition;
 
 /** 
+ * https://live.sioeye.cn/settings#info
 * @ClassName: PersonalProfileCase 
 * @Description: 个人资料
 * @author qiang.zhang@ck-telecom.com
